60e18e2779579598cf8d55239603515d05f2bec6
7HE-LUCKY-FISH <bryanlin1968@gmail.com>
2024-10-28 15:38:38 -0700

    adding the search api. make sure to change the route from the api to what we are using temp is (/api/ *function name*)

--- server/main.py
+++ server/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from auth import auth_bp
+from searchbar import search_bp
+
 
 app = Flask(__name__)
 cors = CORS(app, origins="*")
 
 app.register_blueprint(auth_bp)
-
+app.register_blueprint(search_bp)
 
 @app.route("/api/healthcheck", methods=['GET'])
 def health_check():
