3217738e02429982f9f77106017cccd11770e4b6
Bhuvanesh Selvaraj <bhuvanesh.sel@gmail.com>
2024-10-31 21:03:04 -0700

    Added ability to click on logo to go back to home page

--- client/src/components/NavigationBar/NavigationBar.jsx
+++ client/src/components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,17 @@
 import SearchBar from './SearchBar'
 import './navigation-bar.css'
+import { useNavigate } from 'react-router-dom';
 
 function NavigationBar() {
+    const navigate = useNavigate();
+
+    const goToHome = () => {
+        navigate('/');
+    }
+
     return (
         <header className="navigation-bar">
-            <h1 className="logo">OFS</h1>
+            <h1 className="logo" onClick={goToHome}>OFS</h1>
             <SearchBar />
             <div className="profile-container">
                 <img className="shopping-cart-icon" src="./src/img/shopping-cart-icon.png" alt="" />
