36e260f70b4ea017fbae8f869c3a70352ab4fec8
7HE-LUCKY-FISH <bryanlin1968@gmail.com>
2024-11-06 21:58:12 -0800

    updated the search query to the pattern which Bhuv implemented

--- server/searchbar.py
+++ server/searchbar.py
@@ -8,13 +8,29 @@ search_bp = Blueprint("search", __name__)
 def search_products():
     try:
         # Get search query from request arguments
-        query = request.args.get("query", "").strip()
+        query = request.args.get("q", "").strip()
+        category = request.args.get("category", "").strip()
         
+         # Validate input to prevent SQL injection (remove unwanted characters)
+        if not all(c.isalnum() or c.isspace() for c in query) or not all(c.isalnum() or c.isspace() for c in category):
+            return jsonify({"error": "Invalid characters detected in query or category!"}), 400
+
+
         # Connect to the database
         db_connection = get_db_connection()
         cursor = db_connection.cursor(dictionary=True)
 
-        if query:       
+
+        #updated here to take in both catgory and normal search
+        if query and category:
+            search_query = f"%{query}%"
+            category_query = f"%{category}"
+            cursor.execute("""
+            SELECT product_id, name, brand, stock, price, weight, category, description
+            FROM product
+            WHERE name LIKE %s AND category LIKE %s
+            """, (search_query, category_query))
+        elif query:       
         # Search for products by name or category 
             search_query = f"%{query}%"
             cursor.execute("""
