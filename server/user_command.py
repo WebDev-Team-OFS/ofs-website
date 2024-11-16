b21e5ffc77fc5ff6f2b56545a991010f7f5af81c
7HE-LUCKY-FISH <bryanlin1968@gmail.com>
2024-11-16 12:28:12 -0800

    allow user to see their profile

--- server/user_command.py
+++ server/user_command.py
@@ -3,16 +3,48 @@ from flask import Blueprint, jsonify, request, session, current_app
 from db_module import get_db_connection
 from datetime import timedelta
 
-#we already have cart.py which is broken somewhat so maybe this will get booted also
 
-#for cart presumably
-
+user_cmd_bp = Blueprint('user_command', __name__)
 
 
-#add 
+
+#make sure you use the protected before accesses these
 
 
-#remove
+@user_cmd_bp("api/profile", methods = ['GET'])
+def view_profile():
+    try:
+        if 'user_id' not in session:
+            return jsonify({"error": "Unauthorazied access, login first"}), 401
+        
+        user_id = session['user_id']
+
+        db_connection = get_db_connection()
+        cursor = db_connection.cursor(dictionary=True)
+
+        cursor.execute("""
+                Select user_id, username, first_name, last_name, email data_create, from user_info where user_id = %s  
+                    
+                    """, (user_id))
+        user_profile = cursor.fetchone()
 
 
-#check out counter
+        cursor.close()
+        db_connection.close()
+
+        if user_profile:
+            return jsonify({"profile": user_profile}), 200
+        else:
+            return jsonify({"error": "profile not found"}), 404
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if db_connection:
+            db_connection.close()
+
+
+
+
